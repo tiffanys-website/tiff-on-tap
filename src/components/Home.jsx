191eea838beb09cb98712cc258fad5716cf7ab82
Vonney <davonnejv@gmail.com>
2023-11-24 21:34:15 -0600

    adding different h1 with different tailwind styles and animation

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,14 +3,14 @@ import React from "react";
 export default function Home() {
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center">
-      <h1 className="text-white mb-20 mr-40 text-8xl font-scary">
-        Tiff on Tap
-      </h1>
       <img
         src="images/tiff.png"
         className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:max-w-screen-md"
         alt="Tiff on Tap"
       />
+      <h1 className="text-lime mt-20 mr-20 text-9xl font-scary animate-pulse">
+        Subscribe
+      </h1>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
       fontFamily: {
         scary: "'Creepster', sans-serif",
       },
+      colors: {
+        lime: "#34F924",
+      },
     },
   },
   plugins: [],

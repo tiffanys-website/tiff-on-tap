da3c270c22507ff48d60667b9260759c6630bc2e
Vonney <davonnejv@gmail.com>
2023-11-24 21:44:46 -0600

    adding new image with animate-bounce

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function Home() {
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center">
-      <h1 className="text-white mb-20 mr-40 text-8xl font-scary">
+      <h1 className="text-white mb-20 mr-30 text-8xl font-scary animate-bounce">
         Tiff on Tap
       </h1>
       <img
-        src="images/tiff.png"
-        className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:max-w-screen-md"
+        src="images/white-tappers.jpg"
+        className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:max-w-screen-sm rounded"
         alt="Tiff on Tap"
       />
     </div>

8e8dd5e7bc35ec080da53a061aea7846365a6d3a
Vonney <davonnejv@gmail.com>
2023-11-24 21:27:13 -0600

    adding tailwind styles to h1

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,13 @@ import React from "react";
 export default function Home() {
   return (
     <div className="bg-black h-screen flex flex-col justify-center items-center">
+      <h1 className="text-white mb-20 mr-40 text-8xl font-scary">
+        Tiff on Tap
+      </h1>
       <img
         src="images/tiff.png"
         className="w-full sm:w-1/2 md:w-2/3 lg:w-3/4 xl:max-w-screen-md"
+        alt="Tiff on Tap"
       />
     </div>
   );

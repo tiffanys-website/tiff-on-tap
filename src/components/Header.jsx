5f771673ed64397bbc60514177c2ef1c246909ec
Vonney <davonnejv@gmail.com>
2024-01-07 13:24:41 -0600

    adding Navbar component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
 import { Link } from "react-router-dom";
+import Navbar from "./Navbar";
 
 export default function Header() {
   return (
-    <header className="w-full flex ">
+    <header className="w-full flex justify-center gap-10">
       <Link to="/">
         <img src="images/tiff.png" className="max-w-xs" />
       </Link>
-      <div className="flex justify-center items-center ml-20">
+      <div className="flex justify-center items-center">
         <h1 className="text-center text-green-600 font-scary text-8xl">
           Tiff on Tap
         </h1>
+        <Navbar />
       </div>
     </header>
   );
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <nav className="text-white flex justify-between flex-col lg:flex-row gap-10 ml-10 list-none font-mono">
+      <li>Content </li>
+      <li>Merch</li>
+      <li>Contact</li>
+    </nav>
+  );
+}

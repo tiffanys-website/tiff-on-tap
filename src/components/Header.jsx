19ab4cdf74902fe5cd9dc18137d852d21daf94b3
Vonney <davonnejv@gmail.com>
2024-01-07 12:30:31 -0600

    adding image and link to home page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,16 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <header className="bg-white w-full p-4 ">
-      <h1 className="text-center">Test</h1>
+    <header className="w-full flex ">
+      <Link to="/">
+        <img src="images/tiff.png" className="max-w-xs" />
+      </Link>
+      <div className="flex justify-center items-center ml-20">
+        <h1 className="text-center text-green-600 font-scary text-8xl">
+          Tiff on Tap
+        </h1>
+      </div>
     </header>
   );
 }
-
-// <Link to="/">
-// <img src="images/tiff.png" className="max-w-xs" />
-// </Link>
